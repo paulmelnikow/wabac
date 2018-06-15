d44bbcf24922a1215610e1648fcca45a29ed7828
Paul Melnikow <email@paulmelnikow.com>
2018-06-14 23:43:48 -0400

    Implement cache#put

--- src/cache.js
+++ src/cache.js
@@ -1,10 +1,13 @@
 'use strict'
 
+const crypto = require('crypto')
+const { promisify } = require('util')
+const gzip = promisify(require('zlib').gzip)
 const { isErrorReason } = require('./api-errors')
 
 class Cache {
-  constructor({ storage, bucketName }) {
-    Object.assign(this, { storage, bucketName })
+  constructor({ storage, bucketName, defaultContentType }) {
+    Object.assign(this, { storage, bucketName, defaultContentType })
   }
 
   async initialize({ location }) {
@@ -25,6 +28,10 @@ class Cache {
     }
   }
 
+  get bucket() {
+    return this.storage.bucket(this.bucketName)
+  }
+
   async get(key, { maxAgeSeconds }) {
     throw Error('Not implemented')
   }
@@ -37,8 +44,45 @@ class Cache {
     throw Error('Not implemented')
   }
 
-  async put(key) {
-    throw Error('Not implemented')
+  async put(key, value, options = {}) {
+    const { contentType = this.defaultContentType } = options
+
+    const commonOpts = {
+      gzip: true,
+      // Disable resumable uploads, because they leak state across
+      // processes, leading to unpredictable behavior.
+      // https://github.com/googleapis/nodejs-storage/issues/217
+      resumable: false,
+    }
+
+    // Create a new key.
+    try {
+      await this.bucket.file(key, { generation: 0 }).save(value, {
+        contentType,
+        ...commonOpts,
+      })
+      return
+    } catch (e) {
+      if (!isErrorReason(e, 'conditionNotMet')) {
+        throw e
+      }
+    }
+
+    // Update an existing key.
+    const existing = this.bucket.file(key)
+    const [{ md5Hash: oldHash }] = await existing.getMetadata()
+
+    const newHash = crypto
+      .createHash('md5')
+      .update(await gzip(value))
+      .digest('base64')
+    if (oldHash === newHash) {
+      // Setting the metadata has a side effect of updating the
+      // `metageneration` and the `updated` date.
+      await existing.setMetadata({})
+    } else {
+      await existing.save(value, { ...commonOpts })
+    }
   }
 
   async drop(key) {
--- src/cache.spec.js
+++ src/cache.spec.js
@@ -1,18 +1,23 @@
 'use strict'
 
 const Storage = require('@google-cloud/storage')
+const chai = require('chai')
 const { expect } = require('chai')
+const randomstring = require('randomstring')
 const Cache = require('./cache')
 const { isErrorReason } = require('./api-errors')
 const { projectId, bucketName, location } = require('./test-config')
 
-require('chai').use(require('chai-as-promised'))
+// require('request-debug')(require('request'));
+
+chai.use(require('chai-as-promised'))
+chai.use(require('chai-datetime'))
 
 before('Delete the test bucket', async function() {
-  this.timeout(10000)
-  const storage = new Storage({ projectId })
+  const bucket = new Storage({ projectId }).bucket(bucketName)
   try {
-    await storage.bucket(bucketName).delete()
+    await bucket.deleteFiles({ versions: true })
+    await bucket.delete()
   } catch (e) {
     if (!isErrorReason(e, 'notFound')) {
       throw e
@@ -31,7 +36,6 @@ describe('Cache', function() {
   describe('Initialization', function() {
     describe('initialize', function() {
       it('should create a versioned bucket', async function() {
-        this.timeout(10000)
         await cache.initialize({ location })
 
         const [metadata] = await storage.bucket(bucketName).getMetadata()
@@ -39,7 +43,6 @@ describe('Cache', function() {
       })
 
       it('should error when bucket already exists', async function() {
-        this.timeout(10000)
         await expect(cache.initialize({ location })).to.be.rejectedWith(
           'You already own this bucket. Please select another name.'
         )
@@ -48,8 +51,73 @@ describe('Cache', function() {
 
     describe('initializeIfNeeded', function() {
       it('should not error when bucket already exists', async function() {
-        this.timeout(10000)
         await cache.initializeIfNeeded({ location })
+      })
+    })
+  })
+
+  describe('put', function() {
+    let key
+    let data
+    let cachedFile
+    beforeEach(function() {
+      key = `http://example.com/${randomstring.generate()}.html`
+      data = randomstring.generate()
+      cachedFile = storage.bucket(bucketName).file(key)
+    })
+
+    context('A new key', function() {
+      it('should create the object and assign the content type', async function() {
+        const contentType = 'application/imagined'
+
+        // Act.
+        await cache.put(key, data, { contentType })
+
+        // Assert.
+        const [contents] = await cachedFile.download()
+        expect(contents.toString()).to.equal(data)
+
+        const [
+          { contentType: actualContentType },
+        ] = await cachedFile.getMetadata()
+        expect(actualContentType).to.equal(contentType)
+      })
+    })
+
+    context('An existing key', function() {
+      beforeEach(async function() {
+        await cache.put(key, data)
+      })
+
+      context('with new contents', function() {
+        const newData = randomstring.generate()
+        it('updates the contents', async function() {
+          await cache.put(key, newData)
+
+          const [contents] = await cachedFile.download()
+          expect(contents.toString()).to.equal(newData)
+        })
+      })
+
+      context('with identical contents', function() {
+        it('preserves the generation and updates the date', async function() {
+          const beforeUpdate = new Date()
+
+          const [
+            { generation: beforeGeneration },
+          ] = await cachedFile.getMetadata()
+
+          await cache.put(key, data)
+
+          const afterUpdate = new Date()
+
+          const [
+            { generation: afterGeneration, updated },
+          ] = await cachedFile.getMetadata()
+
+          expect(afterGeneration).to.equal(beforeGeneration)
+          expect(new Date(updated)).to.be.withinDate(beforeUpdate, afterUpdate)
+        })
       })
     })
   })
